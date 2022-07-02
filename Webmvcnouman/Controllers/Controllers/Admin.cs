f8fde171fdb96dcd03d07ee550ca10c0037e7a14
Nomanuskt <103050053+Nomanuskt@users.noreply.github.com>
2022-07-02 19:01:23 +0500

    quiz 2
    quiz 2

---
+++ Webmvcnouman/Controllers/Controllers/Admin.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Webmvcnouman.Controllers
+{
+    public class Admin : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Index1(string fname, string lname, string email, string phone, string gender, string departmwnt)
+        {
+            if (fname == "text" && email == "email")
+            {
+                ViewBag.message = "data recieved";
+            }
+            else
+            {
+                ViewBag.message = "data not recieved";
+            }
+            return View("Index1");
+        }
+
+    }
+
+}
